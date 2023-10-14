b69784419a3b94309d1e1272004fdf2aa835e4c5
Iankoex <koexian@gmail.com>
2023-10-14 15:40:26 +0300

    Initial Commit

---
+++ AudioToTextPlayer/AudioToTextPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioToTextPlayerApp.swift
+//  AudioToTextPlayer
+//
+//  Created by Ian on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AudioToTextPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
